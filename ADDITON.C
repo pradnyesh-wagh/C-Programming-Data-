852bd760456a62f025745b5aff5408377abaa9ae
pradnyesh-wagh <pswagh420@gmail.com>
2022-02-18 18:22:31 +0530

    ADDED: C program of Two Number

---
+++ ADDITON.C
@@ -0,0 +1,11 @@
+//Program - 1  Program for Addtion 2number:
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{	int a=15,b=20,c=0;
+	clrscr();
+	c = a+b;
+	printf("Addition:%d",c);
+	getch();f
+}
