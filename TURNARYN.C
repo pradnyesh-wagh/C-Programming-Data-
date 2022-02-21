0d4871606cc1a45ae6f87d1897f9f6b7414670c0
pradnyesh-wagh <pswagh420@gmail.com>
2022-02-21 17:17:45 +0530

    ADDED: C Program for Turnary Oprator

---
+++ TURNARYN.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n;
+	clrscr();
+	printf("Enter The Number:");
+	scanf("%d",&n);
+
+
+	(n>0)?printf("Number Is Positive:"):printf("Number Is Negative:");
+	getch();
+
+}
